df018cbdc6613969c449e61d9477bf4f01456d36
coti_garcia <javiera.garcialatorre@gmail.com>
2019-09-18 21:42:34 -0400

    create Instruction Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Instructions from "./components/Instructions";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Instructions />
     </div>
   );
 }
