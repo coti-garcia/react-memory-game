1f371196eb72926387056f8ebc8626b30bbb4c66
coti_garcia <javiera.garcialatorre@gmail.com>
2019-09-18 21:56:07 -0400

    footer component + Boostrap added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Instructions from "./components/Instructions";
 import GridImages from "./components/GridImages";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Instructions />
       <GridImages />
+      <Footer />
     </div>
   );
 }
