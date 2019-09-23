809c40faa784d2015f8861aab283010e71d72699
coti_garcia <javiera.garcialatorre@gmail.com>
2019-09-23 18:22:44 -0400

    Updates

---
+++ precache-manifest.5c8ca958316603a357a22651584b5572.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c052dc84a40f857f354e3383664b7c01",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "484b2ee2f8c82029fd07",
+    "url": "/react-memory-game/static/css/main.986c924a.chunk.css"
+  },
+  {
+    "revision": "0e1426cd0da8bf0bc86e",
+    "url": "/react-memory-game/static/js/2.d526cc6c.chunk.js"
+  },
+  {
+    "revision": "484b2ee2f8c82029fd07",
+    "url": "/react-memory-game/static/js/main.26a594a6.chunk.js"
+  },
+  {
+    "revision": "a72a0fc9dd52febb9de8",
+    "url": "/react-memory-game/static/js/runtime~main.e1427b77.js"
+  }
+]);
